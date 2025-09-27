c7ddd4d0bd9b1ec185ea7c9ff4a78e7399dfc12a
Sheng Jiang <shengjiang@cmu.edu>
2025-09-27 18:50:08 -0400

    misc

--- libkineto/src/output_arrow.cpp
+++ libkineto/src/output_arrow.cpp
@@ -22,45 +22,6 @@ namespace KINETO_NAMESPACE {
 // but it would be better not call them in the first place since
 // there are some other logics involved, which are unnecessary.
 
-// std::chrono header start
-#ifdef _GLIBCXX_USE_C99_STDINT_TR1
-#define _KINETO_GLIBCXX_CHRONO_INT64_T int64_t
-#elif defined __INT64_TYPE__
-#define _KINETO_GLIBCXX_CHRONO_INT64_T __INT64_TYPE__
-#else
-#define _KINETO_GLIBCXX_CHRONO_INT64_T long long
-#endif
-// std::chrono header end
-
-// There are tools like Chrome Trace Viewer that uses double to represent
-// each element in the timeline. Double has a 53 bit mantissa to support
-// up to 2^53 significant digits (up to 9007199254740992). This holds at the
-// nanosecond level, about 3 months and 12 days. So, let's round base time to
-// 3 months intervals, so we can still collect traces across ranks relative
-// to each other.
-// A month is 2629746, so 3 months is 7889238.
-using _trimonths =
-    std::chrono::duration<_KINETO_GLIBCXX_CHRONO_INT64_T, std::ratio<7889238>>;
-#undef _GLIBCXX_CHRONO_INT64_T
-
-// The 'ts' field written into the json file has 19 significant digits,
-// while a double can only represent 15-16 digits. By using relative time,
-// other applications can accurately read the 'ts' field as a double.
-// Use the program loading time as the baseline time.
-inline int64_t transToRelativeTime(int64_t time) {
-  // Sometimes after converting to relative time, it can be a few nanoseconds
-  // negative. Since Chrome trace and json processing will throw a parser error,
-  // guard this.
-  static int64_t base_time = libkineto::timeSinceEpoch(
-      std::chrono::time_point<std::chrono::system_clock>(
-          std::chrono::floor<_trimonths>(std::chrono::system_clock::now())));
-  int64_t res = time - base_time;
-  if (res < 0) {
-    return 0;
-  }
-  return res;
-}
-
 static inline int32_t sanitizeTid(int32_t tid) {
   // Convert all negative tids to its positive value. Create a specific case
   // for INT_MIN so it is obvious how it is being handled.
